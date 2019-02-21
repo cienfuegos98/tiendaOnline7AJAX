22ab6ac60bfd8538efcbf64a64b0cf2a03dbda3c
alumno <pgmcastillo98@gmail.com>
2019-02-21 14:12:10 +0100

    empiezo el pago

--- pagar.php
+++ pagar.php
@@ -8,11 +8,31 @@ spl_autoload_register(function($nombre_clase) {
 });
 
 $con = new BD();
+$cesta = new Cesta();
 session_start();
 
 $plantilla = new Smarty();
 $plantilla->template_dir = "./template";
 $plantilla->compile_dir = "./template_c";
+
+$tabla1 = "<table id = 'tablaPagar' class = 'pago'>"
+        . "<thead>"
+        . "<tr class = 'pago'>"
+        . "<th class = 'pago'>Articulo</th>"
+        . "<th class = 'pago'>Cantidad</th>"
+        . "<th class = 'pago'>Precio Unitario</th>"
+        . "</tr>"
+        . "</thead>";
+foreach ($cesta->productos as $codigo => $prods) {
+    $tabla1 .= "<tr class = 'pago'>"
+            . "<td class = 'pago'></td>"
+            . "<td class = 'pago'></td>"
+            . "<td class = 'pago'></td>"
+            . "</tr>";
+}
+$tabla1 .= "</table>";
+
+$plantilla->assign('tabla1', $tabla1);
 
 $plantilla->display("pagar.tpl");
 ?>
