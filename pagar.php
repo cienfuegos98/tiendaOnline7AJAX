ba60c31f898045f0773b3fe07b9c2ad8e798cc59
alumno <pgmcastillo98@gmail.com>
2019-02-22 11:08:24 +0100

    apunto de terminar la ventana de la factura

--- pagar.php
+++ pagar.php
@@ -10,28 +10,44 @@ spl_autoload_register(function($nombre_clase) {
 $con = new BD();
 $cesta = new Cesta();
 session_start();
-
+$tabla1 = "";
+$tabla2 = "";
 $plantilla = new Smarty();
 $plantilla->template_dir = "./template";
 $plantilla->compile_dir = "./template_c";
+$datos_cesta = $_SESSION['cesta'];
 
-$tabla1 = "<table id = 'tablaPagar' class = 'pago'>"
+$tabla1 .= "<table id = 'tablaPagar' class = 'pago'>"
         . "<thead>"
         . "<tr class = 'pago'>"
         . "<th class = 'pago'>Articulo</th>"
         . "<th class = 'pago'>Cantidad</th>"
         . "<th class = 'pago'>Precio Unitario</th>"
         . "</tr>"
-        . "</thead>";
-foreach ($cesta->productos as $codigo => $prods) {
+        . "</thead>"
+        . "<tbody>";
+$arrayProductos = $datos_cesta->getProductos();
+
+
+
+foreach ($arrayProductos as $cod => $prod) {
     $tabla1 .= "<tr class = 'pago'>"
-            . "<td class = 'pago'></td>"
-            . "<td class = 'pago'></td>"
-            . "<td class = 'pago'></td>"
+            . "<td class = 'pago'>" . $cod . "</td>"
+            . "<td class = 'pago'>" . $prod[0] . "</td>"
+            . "<td class = 'pago'>" . $prod[1] . "</td>"
             . "</tr>";
 }
-$tabla1 .= "</table>";
+$tabla1 .= "</tbody></table>";
 
+
+foreach ($arrayProductos as $cod => $prod) {
+    $cantidad += $prod[0];
+}
+
+$total = $cesta->calcularTotal();
+echo "---$total---";
+$plantilla->assign('total', $total);
+$plantilla->assign('cantidad', $cantidad);
 $plantilla->assign('tabla1', $tabla1);
 
 $plantilla->display("pagar.tpl");
